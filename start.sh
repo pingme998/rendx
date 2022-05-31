646627bb8c770ea13d0cbdf3774990428f1d8fb9
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-05-31 17:07:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ apt update
 apt install curl unzip git -y
 curl -L 'rclone.org/install.sh' |bash
 curl -L "$RCLONE_CONF_IN_URL" >/rclone.conf
-rclone rcd --rc-serve --rc-addr=0.0.0.0:8080
+rclone rcd --rc-serve --rc-addr=0.0.0.0:8080 --config=/rclone.conf
+rclone rcd --rc-serve --rc-addr=0.0.0.0:8080 --config=/demo.conf
